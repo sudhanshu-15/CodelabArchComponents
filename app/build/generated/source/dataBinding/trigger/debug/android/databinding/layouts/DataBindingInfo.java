d7eca4dc63ba8246320936b6812cc50d0493f203
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-28 20:32:38 -0400

    Init commit, added Room dependency

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="1c49083b-d4ba-419c-867f-98b805a779ac")
+public class DataBindingInfo {}
