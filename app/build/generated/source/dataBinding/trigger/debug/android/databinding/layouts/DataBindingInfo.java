5dfeeccb5e778e7fcd600e8ac4c5355698fcf344
Sudhanshu Siddh <ssiddh@iu.edu>
2018-04-28 21:47:09 -0400

    Added Sunshine Database with date converters

--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="1c49083b-d4ba-419c-867f-98b805a779ac")
-public class DataBindingInfo {}
